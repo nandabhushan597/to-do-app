7d5ca4614704a404cf950733107cb868f3ba6e3b
Nanda Bhushan <nandabhushan597@gmail.com>
2022-09-04 18:14:35 -0400

    ToDoApp: added buttons and event listeners

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const addTaskButton = document.getElementById('task')
+const addTaskButton = document.getElementById('addButton')
 
 addTaskButton.addEventListener('click', getTask)
+
+const clearTaskButton = document.getElementById('clearButton')
+
+clearTaskButton.addEventListener('click', clearTask)
 
 function getTask() {
     console.log("in get task")
     var taskField = document.getElementById('task').value;
-    var list = document.getElementById('list');
-    list.innerHTML = taskField;
-    console.log(taskField);
+    const para = document.createElement("p");
+    para.innerText = taskField;
+    document.body.appendChild(para);
+    document.getElementById('list').appendChild(para);
+    // list.setAttribute(id)
+    // list.innerHTML = taskField;
+    // console.log(taskField);
+    // create new element 
+    // set id 
+    // set innerHtml 
+}
+
+function clearTask() {
+    console.log("in clear");
+    var taskField = document.getElementById('task');
+    taskField.value = null;
 }
