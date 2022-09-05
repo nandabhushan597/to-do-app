e44a397956e1bd9116c1d59d19ab1d470767c25a
Nanda Bhushan <nandabhushan597@gmail.com>
2022-09-05 15:11:28 -0400

    ToDoApp: working to do app with delete

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+getDate()
+
 const addTaskButton = document.getElementById('addButton')
 
 addTaskButton.addEventListener('click', getTask)
@@ -6,23 +8,39 @@ const clearTaskButton = document.getElementById('clearButton')
 
 clearTaskButton.addEventListener('click', clearTask)
 
+var counter = 0;
+
 function getTask() {
-    console.log("in get task")
+    counter = ++counter; 
+    const attribute = 'para-' + counter;
+    const list = document.getElementById('list');
+
     var taskField = document.getElementById('task').value;
     const para = document.createElement("p");
+    const removeButton = document.createElement("button");
+
+    para.setAttribute('id', attribute);
+    removeButton.setAttribute('id', attribute);
+    removeButton.setAttribute('class', 'removeButton')
+
+    // editButton.innerHTML = "<img src='edit-icon.png'/>";
+    removeButton.innerText = 'Delete';
+    removeButton.onclick = function(){
+        const listItem = document.getElementById(attribute);
+        list.removeChild(listItem);
+    }
     para.innerText = taskField;
-    document.body.appendChild(para);
-    document.getElementById('list').appendChild(para);
-    // list.setAttribute(id)
-    // list.innerHTML = taskField;
-    // console.log(taskField);
-    // create new element 
-    // set id 
-    // set innerHtml 
+
+    list.appendChild(para).appendChild(removeButton);
 }
 
 function clearTask() {
     console.log("in clear");
     var taskField = document.getElementById('task');
     taskField.value = null;
+}
+
+function getDate() {
+    let today = new Date().toLocaleDateString();
+    $(date).text(today)
 }
